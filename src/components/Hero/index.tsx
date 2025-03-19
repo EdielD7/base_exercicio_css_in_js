3e95ba221d186438e360d9c63bca72bb58c1e982
EdielD7 <ediel.purim@hotmail.com>
2025-03-18 23:10:34 -0400

    ajusta global, cabecalho e formVagas

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,13 @@
 import styles from './Hero.module.css'
+import { Container } from '../../styles'
 
 const Hero = () => (
   <form className={styles.form}>
-    <div className="container">
+    <Container>
       <h2 className={styles.heroTitle}>
         As melhores vagas para tecnologia, design e artes visuais.
       </h2>
-    </div>
+    </Container>
   </form>
 )
 
